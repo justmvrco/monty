624938bfe2b26f5a54f6fc66bf0a788ed5bba960
justmvrco <moreirapiliki@gmail.com>
2021-09-22 08:28:42 -0700

    correct mem leaks nad betty

---
+++ nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+  * nop - doesn't do anything.
+  * @stack: double pointer to stack.
+  * @line_number: number of lines.
+  *
+  * Return: void.
+  */
+void nop(stack_t **stack, unsigned int line_number)
+{
+	(void)stack;
+	(void)line_number;
+}
